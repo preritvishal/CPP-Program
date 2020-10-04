cbcb84608c40be06c273f26d5af86debc7e7bd7f
preritvishal <70009672+preritvishal@users.noreply.github.com>
2020-10-04 23:33:40 +0530

    Added ability to clear screen after each choice
    Before moving to the process, added a function call in main and a function names ClearScreen() to clean the console/Screen
    and

--- shoppinglist.cpp
+++ shoppinglist.cpp
@@ -1,13 +1,12 @@
 #include<iostream>
+#include<cstdlib>   // for exit() function
 using namespace std;
 class ITEMS
 {
-
     int itemCode[50];
     float itemPrice[50];
     int count;
 public:
-    char name[50];
     void CNT(void)
     {
         count=0;//initialize count to 0
@@ -16,13 +15,22 @@ public:
 
     void displaysum(void);  // display total value of all items
 
-    void remove(void) ; // delete a specified item
+    void remove_item(void) ; // delete a specified item
 
     void displayIteams(void); // display items
 
 };
+void ScreenClear(){         // function to clean the console/screen
+
+    #ifdef _linux_          // if linux, then execute following command
+            system("clear");
+    #else               // if windows, then execute following command
+            system("cls");
+    #endif
+}
 void ITEMS ::getitem(void)   // assign values to data members of item
 {
+        
         cout<<"Enter item code :";
         cin>>itemCode[count];
         cout<<"Enter item cost:";
@@ -32,13 +40,15 @@ void ITEMS ::getitem(void)   // assign values to data members of item
 }
 void ITEMS::displaysum()  // display total value of all items
  {
+     
         float sum=0;
         for(int i=0;i<count;i++)
             sum+=itemPrice[i];
         cout<<"\nTotal Value :"<<sum<<"\n";
     }
-void ITEMS::remove()  // delete a specified item
+void ITEMS::remove_item()  // delete a specified item
     {
+        
          int a;
          cout<<"Enter item code :";
          cin>>a;
@@ -48,12 +58,13 @@ void ITEMS::remove()  // delete a specified item
     }
     void ITEMS::displayIteams()  // display items
      {
-          cout<<"\n"<<name<<"\n";
-        cout<<"\nCode: Price:";
+         
+        cout<<"\nCode Price\n";
+
         for(int i=0; i<count;i++)
         {
             cout<<"\n"<<itemCode[i];
-            cout<<"   "<<itemPrice[i];
+            cout<<" "<<itemPrice[i];
         }
         cout<<"\n";
     }
@@ -63,26 +74,34 @@ int main()
     order.CNT();
     int x;
     cout<<"*************** WELCOME TO GUDDURYADAV STORES *****************";
-    cout<<"\nEnter A Customer Name :";
-    cin>>order.name;
-        do
+    do
     {
-
+        //ScreenClear(); // to clear screen
         cout<<"\n You can do the following :"<<"Enter appropriate number ";
         cout<<"\n1 : Add an item ";
-         cout<<"\n2 : Display total value ";
-          cout<<"\n3 : Delete an item ";
-           cout<<"\n4 : Display all items ";
-            cout<<"\n5 : Quit ";
-             cout<<"\n\nWhat is your option ?";
-             cin>>x;
+        cout<<"\n2 : Display total value ";
+        cout<<"\n3 : Delete an item ";
+        cout<<"\n4 : Display all items ";
+        cout<<"\n5 : Quit ";
+        cout<<"\n\nWhat is your option ?";
+        cin>>x;
+
              switch(x)
              {
-             case 1 : order.getitem(); break;
-             case 2 : order.displaysum(); break;
-             case 3 : order.remove(); break;
-             case 4 : order.displayIteams(); break;
-             case 5 : break;
+             case 1 : ScreenClear();    // for clearing screen
+                      order.getitem();
+                      break;
+             case 2 : ScreenClear();
+                      order.displaysum();
+                      break;
+             case 3 : ScreenClear();
+                      order.remove_item();
+                      break;
+             case 4 : ScreenClear();
+                      order.displayIteams();\
+                      break;
+             case 5 : ScreenClear();
+                      exit(0);  // for safe exit 
              default : cout<<"Error in input : try again\n";
              }
         }
